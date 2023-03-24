1012a81b6af9ce7982d7b08f06f7c5da09a893a1
zrminkim <zrminkim@gmail.com>
2023-03-24 16:34:17 +0900

    패키지의 클래스 다 넣었다 개힘드네

---
+++ src/main/java/shop/model/auction/Test.java
@@ -0,0 +1,7 @@
+package shop.model.auction;
+
+public class Test {
+	public void sss() {
+		System.out.println("성민아 보이니");
+	}
+}
---
+++ src/main/java/shop/model/cart/Test.java
@@ -0,0 +1,7 @@
+package shop.model.cart;
+
+public class Test {
+	public void sss() {
+		System.out.println("성민아 보이니");
+	}
+}
